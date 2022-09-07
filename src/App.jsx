24a87722dd9365dcd2d148ba1ebe5689a7e37231
Melin Kan <melin.kan@outlook.com>
2022-09-07 14:06:13 +0300

    Set-up react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home/index";
 import AboutUs from "./pages/AboutUs";
 import Navbar from "./components/Navbar";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-      </Routes>
+      <QueryClientProvider queryClient={queryClient}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
