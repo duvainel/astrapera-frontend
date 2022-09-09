875973bbf8af773ce56f919dfa52606de6474216
Melin Kan <melin.kan@outlook.com>
2022-09-10 01:48:18 +0300

    Create Navbar component

---
+++ src/components/Navbar/MobileNav.jsx
@@ -0,0 +1,79 @@
+import {
+  Flex,
+  Text,
+  Stack,
+  Collapse,
+  Icon,
+  useColorModeValue,
+  useDisclosure,
+  Link,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { LinkWithRouter } from "../../components";
+
+export function MobileNav(props) {
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      p={4}
+      display={{ md: "none" }}
+    >
+      {props.items.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+}
+
+function MobileNavItem({ label, children, href }) {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? "#"}
+        justify={"space-between"}
+        align={"center"}
+        _hover={{
+          textDecoration: "none",
+        }}
+      >
+        <Text
+          fontWeight={600}
+          color={useColorModeValue("gray.600", "gray.200")}
+        >
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition={"all .25s ease-in-out"}
+            transform={isOpen ? "rotate(180deg)" : ""}
+            w={6}
+            h={6}
+          />
+        )}
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+          align={"start"}
+        >
+          {children &&
+            children.map((child) => (
+              <LinkWithRouter key={child.label} py={2} href={child.href}>
+                {child.label}
+              </LinkWithRouter>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+}
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,30 +1,117 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import logo from "../../assets/logo.webp";
-import { Image, Flex, Spacer, Box, List, ListItem } from "@chakra-ui/react";
-import { useGetPostsQuery } from "../../state/baseApi";
+import {
+  Box,
+  Flex,
+  IconButton,
+  Collapse,
+  useColorModeValue,
+  useDisclosure,
+  Image,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { LinkWithRouter, Container } from "../../components";
+import { DesktopNav } from "./DesktopNav";
+import { MobileNav } from "./MobileNav";
+import { useGetCategoriesQuery } from "../../state/baseApi";
+import { useMemo, useCallback, useEffect } from "react";
 
-export default function Navbar() {
-  const { data } = useGetPostsQuery();
-  console.log(data);
+export function Navbar() {
+  const { isOpen, onToggle } = useDisclosure();
+  const { data: categories } = useGetCategoriesQuery();
+
+  const NAV_ITEMS = useMemo(() => {
+    return [
+      {
+        label: "Ana Sayfa",
+        href: "/",
+      },
+      {
+        label: "Hakkımızda",
+        href: "/hakkimizda",
+      },
+      {
+        label: "Kategoriler",
+        children: [
+          {
+            label: "Tüm Yazılar",
+            href: "/kategoriler/0",
+          },
+        ],
+      },
+    ];
+  }, [categories]);
+
+  const generateCategories = useCallback(() => {
+    const navCategories = NAV_ITEMS.filter(
+      (item) => item.label === "Kategoriler"
+    );
+
+    const generatedCategories = categories?.data.map((category) => {
+      navCategories[0].children.push({
+        label: category.attributes.name,
+        href: `/kategoriler/${category.id}`,
+      });
+    });
+
+    navCategories.children = generatedCategories;
+  }, [categories]);
+
+  generateCategories();
+
   return (
-    <Flex as={"nav"} minWidth="max-content" alignItems="center" gap="2">
-      <Box>
-        <Link to={"/"}>
-          <Image width="100px" objectFit="cover" src={logo} alt="Dan Abramov" />
-        </Link>
-      </Box>
-      <Spacer />
-      <Box color="brand.300">
-        <List display="flex" gap={3}>
-          <ListItem>
-            <Link to={"/"}>Ana Sayfa</Link>
-          </ListItem>
-          <ListItem>
-            <Link to={"/AboutUs"}>Hakkımızda</Link>
-          </ListItem>
-        </List>
-      </Box>
-    </Flex>
+    <Box
+      borderBottom={1}
+      borderStyle={"solid"}
+      borderColor={useColorModeValue("gray.200", "gray.900")}
+    >
+      <Container>
+        <Flex
+          bg={useColorModeValue("white", "gray.800")}
+          color={useColorModeValue("gray.600", "white")}
+          minH={"60px"}
+          py={{ base: 2 }}
+          align={"center"}
+          justify={"space-between"}
+          direction={{ base: "row-reverse", md: "row" }}
+        >
+          <Flex ml={{ base: -2 }} display={{ base: "flex", md: "none" }}>
+            <IconButton
+              onClick={onToggle}
+              icon={
+                isOpen ? (
+                  <CloseIcon w={3} h={3} />
+                ) : (
+                  <HamburgerIcon w={5} h={5} />
+                )
+              }
+              variant={"ghost"}
+              aria-label={"Toggle Navigation"}
+            />
+          </Flex>
+          <Flex
+            flex={{ md: 1 }}
+            justify={{ base: "center", md: "space-between" }}
+            align={"center"}
+          >
+            <LinkWithRouter href={"/"}>
+              <Image
+                width="90px"
+                objectFit="cover"
+                src={logo}
+                alt="Dan Abramov"
+              />
+            </LinkWithRouter>
+
+            <Flex display={{ base: "none", md: "flex" }} ml={10}>
+              <DesktopNav items={NAV_ITEMS} />
+            </Flex>
+          </Flex>
+        </Flex>
+
+        <Collapse in={isOpen} animateOpacity>
+          <MobileNav items={NAV_ITEMS} />
+        </Collapse>
+      </Container>
+    </Box>
   );
 }
