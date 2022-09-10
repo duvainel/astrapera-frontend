363c5b96490cc84ecc598f385b8393c040e160d5
Melin Kan <melin.kan@outlook.com>
2022-09-10 13:13:48 +0300

    Reduce padding in mobile

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 export function Container(props) {
   return (
-    <Box maxW={1280} mx="auto" px={10} {...props}>
+    <Box maxW={1280} mx="auto" px={{ base: 5, md: 10 }} {...props}>
       {props.children}
     </Box>
   );
