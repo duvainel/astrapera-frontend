17bb93c221bbe7cbfe520aa964b4132b326200cd
Melin Kan <melin.kan@outlook.com>
2022-09-10 14:02:39 +0300

    Implement blog detail with react-markdown and prose

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { withProse } from "@nikolovlazar/chakra-ui-prose";
 
-const theme = extendTheme({
-  colors: {
-    brand: {
-      300: "#ba96d1",
-      400: "#5a3977",
-      500: "#411b63",
-    },
-    text: {
-      dark: "#6f7071",
-      light: "#e5e0d8",
+const theme = extendTheme(
+  {
+    colors: {
+      brand: {
+        300: "#ba96d1",
+        400: "#5a3977",
+        500: "#411b63",
+      },
+      text: {
+        dark: "#6f7071",
+        light: "#e5e0d8",
+      },
     },
   },
-});
+  withProse()
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/pages/BlogDetail/index.jsx
+++ src/pages/BlogDetail/index.jsx
@@ -1,6 +1,17 @@
 import { useParams } from "react-router-dom";
+import { useGetPostQuery } from "../../state/baseApi";
+import { Prose } from "@nikolovlazar/chakra-ui-prose";
+import ReactMarkdown from "react-markdown";
 
 export function BlogDetail() {
   const { id } = useParams();
-  return <div>Blog ID: {id}</div>;
+  const { data: post } = useGetPostQuery(id);
+
+  if (!post) return <div>Loading...</div>;
+
+  return (
+    <Prose>
+      <ReactMarkdown>{post.data.attributes.description}</ReactMarkdown>
+    </Prose>
+  );
 }
