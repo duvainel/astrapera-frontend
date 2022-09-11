a05f4ab17fab411412fe19ace3d2cdb6c2a5e889
Melin Kan <melin.kan@outlook.com>
2022-09-11 13:34:05 +0300

    Set environment

--- src/state/baseApi.js
+++ src/state/baseApi.js
@@ -1,18 +1,17 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import qs from "qs";
 
+const { VITE_APP_BASE_URL, VITE_APP_API_TOKEN } = import.meta.env;
+
 export const baseApi = createApi({
   reducerPath: "astraperaApi",
   baseQuery: fetchBaseQuery({
     paramsSerializer(params) {
       return qs.stringify(params, { encodeValuesOnly: true });
     },
-    baseUrl: "http://localhost:1337/api",
+    baseUrl: VITE_APP_BASE_URL,
     prepareHeaders: (headers) => {
-      headers.set(
-        "Authorization",
-        "Bearer 5241c34853b775ace67617709edf4719e67cab5db1831dab74beceeec05dfe8020011bbb10b0c35ba45f4f80db2bdcea7dffb3dbf9f9efec2e0a9f17c42973b325c78ce8a9b03ee634d4254b946a0d8177ed7233085537f28929c21518908d87b1cbb46decaf70caa04d1ce48458e5b4073452ab1cacbd086b3c75512ad69469"
-      );
+      headers.set("Authorization", `Bearer ${VITE_APP_API_TOKEN}`);
       return headers;
     },
   }),
